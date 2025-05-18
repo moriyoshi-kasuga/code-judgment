f84c658e541d6352c117ceb279fa38522b6e4504
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-18 19:19:04 +0900

    feat: add gcc14 for rustc182Env

--- runner/default.nix
+++ runner/default.nix
@@ -1,7 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  rustc182Env = pkgs.buildEnv {
+    name = "rustc182Env";
+    paths = [ pkgs.rustc pkgs.gcc14 ];
+  };
+in
 {
-  rustc182Env = pkgs.rustc;
+  inherit rustc182Env;
   go123Env = pkgs.go_1_23;
   python313Env = pkgs.python313;
 }
