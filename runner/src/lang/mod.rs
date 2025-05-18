6a139519263290f23a69c8cd65f143cc1a925895
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-18 19:19:04 +0900

    fix: some code on lang

--- runner/src/lang/mod.rs
+++ runner/src/lang/mod.rs
@@ -50,7 +50,7 @@ pub fn lang_into_runner(lang: Language) -> &'static LangRunner {
     match lang {
         Language::Rust1_82 => &LangRunner::WithCompile {
             file_name: "main.rs",
-            compile_cmd: "rustc --release main.rs -o main",
+            compile_cmd: "rustc -C opt-level=3 main.rs -o main",
             run_cmd: "./main",
         },
         Language::Go1_23 => &LangRunner::WithCompile {
@@ -60,7 +60,7 @@ pub fn lang_into_runner(lang: Language) -> &'static LangRunner {
         },
         Language::Python3_13 => &LangRunner::WithoutCompile {
             file_name: "main.py",
-            run_cmd: "python3 main.py",
+            run_cmd: "python main.py",
         },
     }
 }
