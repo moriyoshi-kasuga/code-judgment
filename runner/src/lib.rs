cec000eb956727b83373a48a8be07dc428ddf2a3
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-18 19:19:04 +0900

    wip: start impl runner

--- runner/src/lib.rs
+++ runner/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use runner_schema::web::{RunnerRequest, RunnerResponse};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod error;
+pub use error::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn runner(request: RunnerRequest) -> Result<RunnerResponse> {
+    todo!();
 }
