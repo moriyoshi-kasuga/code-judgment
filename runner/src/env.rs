d4d9a49096c1fcbdad19debc521905d408275391
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-18 21:57:12 +0900

    fix: remove USE_CGROUP_V2

--- runner/src/env.rs
+++ runner/src/env.rs
@@ -1,8 +1,5 @@
 #[derive(Debug, envman::EnvMan)]
-pub struct RunnerEnv {
-    #[envman(default = true)]
-    pub use_cgroup_v2: bool,
-}
+pub struct RunnerEnv {}
 
 pub const RUNNER_PATH: &str = "/runner";
 pub const RUNNING_PATH: &str = "/running";
